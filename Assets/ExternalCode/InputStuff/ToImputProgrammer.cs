fc2635358ff85e9de27916169a526e98457c7943
Jack Shi <shisoftgenius@gmail.com>
2016-06-25 23:13:20 +0800

    rerearrange code

--- Assets/ExternalCode/InputStuff/ToImputProgrammer.cs
+++ Assets/ExternalCode/InputStuff/ToImputProgrammer.cs
@@ -53,14 +53,7 @@ public class InputManager {
 		return freqIdx;
 	}
 
-	/// <summary>
-	/// 画面更新时调用一次，主线程
-	/// 暂停时也会持续调用，谱面尚未加载时也会持续调用，下同
-	/// 画面更新 是指 Unity显示层（以玩家眼中的游戏画面为准）更新，距离上次调用的时间为 UnityEngine.Time.deltaTime
-	/// 当前歌曲播放的进度（秒）为 TentacleGuitarUnity.Stage.Time，暂停时持续刷新同一个秒数
-	/// 请适当 cache 节约性能
-	/// </summary>
-	public static void StageUpdate () {
+	public static List<InNote> getNotes (){
 		int bins = 8192;
 		float[] spectrum = StageMicrophone.GetSpectrumData (bins);
 		if (spectrum != null) {
@@ -90,6 +83,21 @@ public class InputManager {
 			if (noteList.Count > maxNotesInChord) {
 				noteList.RemoveRange (maxNotesInChord, noteList.Count - maxNotesInChord);
 			}
+			return noteList;
+		}
+		return null;
+	}
+
+	/// <summary>
+	/// 画面更新时调用一次，主线程
+	/// 暂停时也会持续调用，谱面尚未加载时也会持续调用，下同
+	/// 画面更新 是指 Unity显示层（以玩家眼中的游戏画面为准）更新，距离上次调用的时间为 UnityEngine.Time.deltaTime
+	/// 当前歌曲播放的进度（秒）为 TentacleGuitarUnity.Stage.Time，暂停时持续刷新同一个秒数
+	/// 请适当 cache 节约性能
+	/// </summary>
+	public static void StageUpdate () {
+		var noteList = getNotes();
+		if (noteList != null) {
 			var strNoteList = new List<string> (); 
 			foreach (InNote note in noteList) {
 				strNoteList.Add (NotesMap.notes[note.Id]);
@@ -99,34 +107,34 @@ public class InputManager {
 	}
 
 
-		/// <summary>
-		/// Unity物理层、逻辑层更新时调用一次
-		/// 多次调用的时间间隔相等，大约为0.02s，具体时间用 UnityEngine.Time.deltaTime 可以获取 
-		/// 主线程假死会使时间间隔变大，此时 deltaTime 有一定几率依旧显示正常
-		/// </summary>
-		public static void StageFixedUpdate () {
+	/// <summary>
+	/// Unity物理层、逻辑层更新时调用一次
+	/// 多次调用的时间间隔相等，大约为0.02s，具体时间用 UnityEngine.Time.deltaTime 可以获取 
+	/// 主线程假死会使时间间隔变大，此时 deltaTime 有一定几率依旧显示正常
+	/// </summary>
+	public static void StageFixedUpdate () {
 
-			// ----- Your Code Here -----
+		// ----- Your Code Here -----
 
-		}
+	}
 
 
 	#endregion
 
 
-		#region -------- Stage API ---------
+	#region -------- Stage API ---------
 
 
-		// 用 Stage.Beat(int, int, float) 单次击打一下某个位置，
+	// 用 Stage.Beat(int, int, float) 单次击打一下某个位置，
 
-		// 用 StageInput.Hold(int, int) 按住某个位置一帧，确保在 StageUpdate 函数里调用
+	// 用 StageInput.Hold(int, int) 按住某个位置一帧，确保在 StageUpdate 函数里调用
 
-		// 用 StageMicrophone.SamplePosition() 获取当前麦克风正在录制的位置，单位Sample（秒x44100)
+	// 用 StageMicrophone.SamplePosition() 获取当前麦克风正在录制的位置，单位Sample（秒x44100)
 
-		// 用 StageMicrophone.GetData() 获取一段已经录制的音频，这个音频是声音最原始的震动，采样率44100次每秒。
-
-
-		#endregion
+	// 用 StageMicrophone.GetData() 获取一段已经录制的音频，这个音频是声音最原始的震动，采样率44100次每秒。
 
 
-	}
+	#endregion
+
+
+}

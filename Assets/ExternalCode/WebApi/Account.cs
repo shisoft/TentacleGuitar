87b782d541ce87f59ef8a5ff5c08d0e4320c17cf
あまみや ゆうこ <1@1234.sh>
2016-06-25 13:17:32 +0800

    Adding netwrok infrastructures

---
+++ Assets/ExternalCode/WebApi/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.ExternalCode.WebApi
+{
+    public static class Account
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="Username">用户名</param>
+        /// <param name="Password">密码</param>
+        /// <returns>成功返回用户Token，失败返回null</returns>
+        public static string SignIn(string Username, string Password)
+        {
+            var args = new Dictionary<string, string>();
+            args.Add("Username", Username);
+            args.Add("Password", Password);
+            var ret = HttpHelper.HttpPost("http://tentacleguitar.azurewebsites.net/SignIn", args);
+            if (ret == "Access Denied")
+                return null;
+            else
+                return ret;
+        }
+    }
+}
---
+++ Assets/ExternalCode/WebApi/HttpHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Assets.ExternalCode.WebApi
+{
+    public static class HttpHelper
+    {
+        public static CookieContainer cookie = new CookieContainer();
+        public static string HttpPost(string Url, Dictionary<string, string> args)
+        {
+            string ret = string.Empty;
+            try
+            {
+                string Param = string.Empty;
+                foreach (var arg in args)
+                {
+                    Param += String.Format("{0}={1}&", arg.Key, arg.Value);
+                }
+                Param = Param.TrimEnd('&');
+                byte[] byteArray = Encoding.Default.GetBytes(Param);
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(Url));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/x-www-form-urlencoded";
+                webReq.CookieContainer = cookie;
+                webReq.ContentLength = byteArray.Length;
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                response.Cookies = webReq.CookieContainer.GetCookies(new Uri(Url));
+
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.Default);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch
+            {
+            }
+            return ret;
+        }
+        public static string HttpGet(string Url)
+        {
+            string ret = string.Empty;
+            try
+            {
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(Url));
+                webReq.Method = "GET";
+                webReq.CookieContainer = cookie;
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.Default);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+            }
+            catch
+            {
+            }
+            return ret;
+        }
+    }
+}

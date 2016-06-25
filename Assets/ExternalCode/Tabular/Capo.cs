4b70237586b6e4bb2da13fe69a53c1ae33e152be
あまみや ゆうこ <1@1234.sh>
2016-06-25 14:12:45 +0800

    Fixed capo model

--- Assets/ExternalCode/Tabular/Capo.cs
+++ Assets/ExternalCode/Tabular/Capo.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TentacleGuitar.Tabular
+﻿namespace TentacleGuitar.Tabular
 {
     public class Capo
     {
-        public int From { get; set; } = 1;
+        public int From { get; set; }
 
-        public int To { get; set; } = 6;
+        public int To { get; set; }
 
-        public int Fret { get; set; } = 0;
+        public int Fret { get; set; }
     }
 }

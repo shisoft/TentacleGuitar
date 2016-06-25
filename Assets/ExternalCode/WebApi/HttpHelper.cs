eb293edd0095aae9323a19a9de7996f59c39e4f8
あまみや ゆうこ <1@1234.sh>
2016-06-25 21:43:18 +0800

    Increased buffer size

--- Assets/ExternalCode/WebApi/HttpHelper.cs
+++ Assets/ExternalCode/WebApi/HttpHelper.cs
@@ -11,7 +11,7 @@ namespace Assets.ExternalCode.WebApi
     {
         public static void CopyTo(this Stream input, Stream output)
         {
-            byte[] buffer = new byte[16 * 1024]; // Fairly arbitrary size
+            byte[] buffer = new byte[16 * 1024 * 1024]; // Fairly arbitrary size
             int bytesRead;
 
             while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
